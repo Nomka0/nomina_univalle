fd70a7bd84c3bca7abd25f799066f1ca93b95b41
nomka0 <juanjose111102@gmail.com>
2023-06-27 15:15:09 -0500

    clase TarifaCana

--- src/modelo/TarifaCana.java
+++ src/modelo/TarifaCana.java
@@ -2,46 +2,67 @@ package modelo;
 import java.util.HashMap;
 import java.util.Objects;
 
-//clase para las tarifas de las cañas, esta info le conscierne a ConceptoDevengo, para calcular los devengos
+//clase para las tipos de las cañas, esta info le conscierne a ConceptoDevengo, para calcular los devengos
 public class TarifaCana {
-	HashMap<Pair, Integer> tarifas = new HashMap<>(); //tarifas de caña, el pair es para
-	//señalar si el día es festivo u ordinario y también el tipo decaña, y el integer señala el tipo de tarifa
-
+	HashMap<Pair, Integer> tipos = new HashMap<>(); //tipos de tipo caña, el pair es para
+	//señalar si el día es festivo u ordinario y también el tipo decaña, y el integer señala el tipo de tipo
+	HashMap<Integer,Integer> tarifas = new HashMap<>();
+	
 	public TarifaCana() {
-		tarifas.put(new Pair(1, 'O'), 1); //quemada ordinaria
-		tarifas.put(new Pair(2, 'F'), 2); //quemada festiva
-		tarifas.put(new Pair(3, 'O'), 3);//cruda ordinaria 
-		tarifas.put(new Pair(4, 'F'), 4);//cruda festiva
+		//inicialización tipos
+		tipos.put(new Pair(1, 'O'), 1); //quemada ordinaria
+		tipos.put(new Pair(2, 'F'), 2); //quemada festiva
+		tipos.put(new Pair(3, 'O'), 3);//cruda ordinaria 
+		tipos.put(new Pair(4, 'F'), 4);//cruda festiva
+
+		//inicialización tarifas
+		tarifas.put(1, 300000);//el dato de clave es el tipo de tarifa, el dato a acceder es el valor en pesos
+		tarifas.put(2, 400000);
+		tarifas.put(3, 800000);
+		tarifas.put(4, 1000000);
 	}
 
-	public int getTarifa(int cana_tipo, char dia_tipo) {
-		int tarifa = tarifas.get(new Pair(cana_tipo, dia_tipo));
-		getTarifaString(tarifa);
-		return tarifa;
+	public int getTipo(int cana_tipo, char dia_tipo) {
+		int tipo = tipos.get(new Pair(cana_tipo, dia_tipo));
+		getTipoString(tipo);
+		return tipo;
 	}
 
 
-	public void getTarifaString(int tarifa) {
-		switch(tarifa) {
+	public void getTipoString(int tipo) {
+		switch(tipo) {
 		case 1:
-			System.out.println("quemada ordinaria");
+			System.out.println("el tipo de tarifa es: quemada ordinaria");
 			break;
 		case 2:
-			System.out.println("quemada festiva");
+			System.out.println("el tipo de tarifa es: quemada festiva");
 			break;
 		case 3:
-			System.out.println("cruda ordinaria");
+			System.out.println("el tipo de tarifa es: cruda ordinaria");
 			break;
 		case 4:
-			System.out.println("cruda festiva");
+			System.out.println("el tipo de tarifa es: cruda festiva");
 			break;
 		default:
 			// Código para otros casos
 			System.out.println("invalido");
 			break;
 		}
+		
+	}
+	
+	public int getTarifa(int cana_tipo, char dia_tipo) {
+		int tipo = getTipo(cana_tipo, dia_tipo);
+		int tarifa = tarifas.get(tipo);
+		
+		getTipoString(tipo);
+		System.out.println("el valor de tarifa es " + tipo);
+		return tarifa;
 	}
 
+}
+
+//para almacenar el primer dato de la clave del hashmap, el cual se conforma de una pareja de int y char
 	class Pair {
 		private int key1;
 		private char key2;
@@ -65,4 +86,3 @@ public class TarifaCana {
 			return Objects.hash(key1, key2);
 		}
 	}
-}
