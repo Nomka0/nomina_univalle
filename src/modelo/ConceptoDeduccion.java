cb6784a79e320b5d314d8ffea5d70b966c2896d8
Daniel <84422391+Daniel8900259@users.noreply.github.com>
2023-06-30 13:44:52 -0500

    Update ConceptoDeduccion.java

--- src/modelo/ConceptoDeduccion.java
+++ src/modelo/ConceptoDeduccion.java
@@ -1,17 +1,34 @@
 package modelo;
 
-public class ConceptoDeduccion extends Entidad{
-	int codigo;
-	String nombre;
-	
-	public ConceptoDeduccion(int codigo, String nombre) {
-		super(codigo, nombre);
-	}
-	
-	public ConceptoDeduccion() {
-		super();
-	}
-
+public class ConceptoDeduccion extends Entidad {
+    private int codigo;
+    private String nombre;
+    
+    public ConceptoDeduccion(int codigo, String nombre) {
+        super(codigo, nombre);
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
+    
+    public ConceptoDeduccion() {
+        super();
+    }
+    
+    public int getCodigo() {
+        return codigo;
+    }
+    
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
 
 
