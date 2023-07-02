ad021e9d9f8b23c41d93cea70e30794cafdc01bb
Daniel <84422391+Daniel8900259@users.noreply.github.com>
2023-07-01 23:37:03 -0500

    Update ConceptoDeduccionControlador.java

--- src/controlador/ConceptoDeduccionControlador.java
+++ src/controlador/ConceptoDeduccionControlador.java
@@ -1,35 +1,132 @@
 package controlador;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 import dao.ConceptoDeduccionDAO;
-import java.util.List;
 import modelo.ConceptoDeduccion;
 
 public class ConceptoDeduccionControlador {
-    private ConceptoDeduccionDAO conceptoDeduccionDAO;
+    private String carpeta;
+    private File[] archivos = {};
+    private Map<Integer, ConceptoDeduccionDAO> empleadosDAOS;
 
     public ConceptoDeduccionControlador() {
-        conceptoDeduccionDAO = new ConceptoDeduccionDAO();
+        carpeta = "CSVs/empleados/";
+        empleadosDAOS = new HashMap<>();
     }
 
-    public void crearConceptoDeduccion(int codigo, String nombre) {
-        ConceptoDeduccion conceptoDeduccion = new ConceptoDeduccion(codigo, nombre);
-        conceptoDeduccionDAO.crear(conceptoDeduccion);
+    public Map<Integer, ConceptoDeduccionDAO> getMapDeduccionesDAO() {
+        return empleadosDAOS;
     }
 
-    public void actualizarConceptoDeduccion(int index, int nuevoCodigo, String nuevoNombre) {
-        ConceptoDeduccion conceptoDeduccion = new ConceptoDeduccion(nuevoCodigo, nuevoNombre);
-        conceptoDeduccionDAO.actualizar(index, conceptoDeduccion);
+    public ConceptoDeduccionDAO seleccionarDAO(int ID) {
+        return empleadosDAOS.get(ID);
     }
 
-    public void eliminarConceptoDeduccion(int index) {
-        conceptoDeduccionDAO.eliminar(index);
+    public void listarArchivos(int id_empleado) {
+        File directorio = new File(carpeta + id_empleado + "/deducciones");
+
+        if (directorio.isDirectory()) {
+            archivos = directorio.listFiles();
+            reverseFileArray(archivos);
+
+            if (archivos != null) {
+                for (File archivo : archivos) {
+                    System.out.println("Nombre del archivo: " + archivo.getName());
+                }
+            } else {
+                System.out.println("La carpeta está vacía o no se pudo acceder a los archivos.");
+            }
+        } else {
+            System.out.println("La ruta especificada no es una carpeta válida.");
+        }
     }
 
-    public ConceptoDeduccion obtenerConceptoDeduccion(int index) {
-        return conceptoDeduccionDAO.obtener(index);
+    public int[] getEmpleadosDir() {
+        File directorio = new File(carpeta);
+        File[] archivos = directorio.listFiles();
+
+        if (directorio.isDirectory()) {
+            Arrays.sort(archivos, (a, b) -> b.getName().compareTo(a.getName()));
+
+            int[] nombres_subdirectorios = new int[archivos.length];
+            for (int i = 0; i < archivos.length; i++) {
+                if (archivos[i].isDirectory()) {
+                    try {
+                        nombres_subdirectorios[i] = Integer.parseInt(archivos[i].getName());
+                        System.out.println("Directorio: " + nombres_subdirectorios[i]);
+                    } catch (NumberFormatException e) {
+                        System.out.println("El nombre del subdirectorio no es un número válido.");
+                    }
+                }
+            }
+            return nombres_subdirectorios;
+        } else {
+            System.out.println("La ruta no corresponde a un directorio válido.");
+            return new int[0];
+        }
     }
 
-    public List<ConceptoDeduccion> obtenerTodosConceptosDeduccion() {
-        return conceptoDeduccionDAO.obtenerTodos();
+    public void leerSubArchivos() {
+        int[] directorios_empleados = getEmpleadosDir();
+
+        for (int directorio : directorios_empleados) {
+            empleadosDAOS.put(directorio, leerArchivo(directorio));
+        }
+        System.out.println(empleadosDAOS);
+    }
+
+    public ConceptoDeduccionDAO leerArchivo(int id_empleado) {
+        listarArchivos(id_empleado);
+        ConceptoDeduccionDAO nuevo_deduccion = new ConceptoDeduccionDAO();
+
+        for (File ruta_archivo : archivos) {
+            boolean primeraLinea = true;
+            try {
+                Scanner scanner = new Scanner(ruta_archivo);
+
+                while (scanner.hasNextLine()) {
+                    String linea = scanner.nextLine();
+
+                    if (primeraLinea) {
+                        primeraLinea = false;
+                        continue;
+                    }
+
+                    String[] campos = linea.split(",");
+                    int ficha = Integer.parseInt(campos[0]);
+                    String fecha = campos[1];
+                    String concepto = campos[2];
+                    float monto = Float.parseFloat(campos[3]);
+
+                    ConceptoDeduccion deduccion = new ConceptoDeduccion(ficha, concepto, fecha, monto);
+                    nuevo_deduccion.crear(deduccion);
+                }
+
+                scanner.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        return nuevo_deduccion;
+    }
+
+    public static void reverseFileArray(File[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            File temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
     }
 }
