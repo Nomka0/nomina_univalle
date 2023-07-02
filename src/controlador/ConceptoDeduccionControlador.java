330cde405c7542bd40a5b3c12a5c637bd137d581
Daniel <84422391+Daniel8900259@users.noreply.github.com>
2023-07-02 18:24:34 -0500

    Update ConceptoDeduccionControlador.java

--- src/controlador/ConceptoDeduccionControlador.java
+++ src/controlador/ConceptoDeduccionControlador.java
@@ -1,25 +1,20 @@
 package controlador;
 
+import dao.ConceptoDeduccionDAO;
+import modelo.ConceptoDeduccion;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-import dao.ConceptoDeduccionDAO;
-import modelo.ConceptoDeduccion;
-
-import java.util.Arrays;
-import java.util.HashMap;
-
 public class ConceptoDeduccionControlador {
     private String carpeta;
-    private File[] archivos = {};
     private Map<Integer, ConceptoDeduccionDAO> empleadosDAOS;
 
     public ConceptoDeduccionControlador() {
@@ -28,7 +23,7 @@ public class ConceptoDeduccionControlador {
     }
 
     public Map<Integer, ConceptoDeduccionDAO> getMapDeduccionesDAO() {
-        return empleadosDAOS; 
+        return empleadosDAOS;
     }
 
     public ConceptoDeduccionDAO seleccionarDAO(int ID) {
@@ -36,130 +31,107 @@ public class ConceptoDeduccionControlador {
     }
 
     public void listarArchivos(int id_empleado) {
-        File directorio = new File(carpeta + id_empleado + "/deducciones");
-        if (directorio.isDirectory()) {
-            archivos = directorio.listFiles();
-            reverseFileArray(archivos);
-            if (archivos != null) {
-                for (File archivo : archivos) {
-                    System.out.println("Nombre del archivo: " + archivo.getName());
-                }
-            } else {
-                System.out.println("La carpeta está vacía o no se pudo acceder a los archivos.");
-            }
+        ConceptoDeduccionDAO dao = seleccionarDAO(id_empleado);
+        List<String> archivos = dao.getArchivos();
+
+        if (archivos.isEmpty()) {
+            System.out.println("No hay archivos para el empleado con ID: " + id_empleado);
         } else {
-            System.out.println("La ruta especificada no es una carpeta válida.");
-        }
-    }
-
-    public int[] getEmpleadosDir() {
-        File directorio = new File(carpeta);
-        File[] archivos = directorio.listFiles();
-
-        if (directorio.isDirectory()) {
-            Arrays.asList(archivos).sort((a, b) -> b.getName().compareTo(a.getName()));
-
-            int[] nombres_subdirectorios = new int[archivos.length];
-            for (int i = 0; i < archivos.length; i++) {
-                if (archivos[i].isDirectory()) {
-                    try {
-                        nombres_subdirectorios[i] = Integer.parseInt(archivos[i].getName());
-                        System.out.println("Directorio: " + nombres_subdirectorios[i]);
-                    } catch (NumberFormatException e) {
-                        System.out.println("El nombre del subdirectorio no es un número válido.");
-                    }
-                }
+            System.out.println("Archivos del empleado con ID: " + id_empleado);
+            for (String archivo : archivos) {
+                System.out.println(archivo);
             }
-            return nombres_subdirectorios;
-        } else {
-            System.out.println("La ruta no corresponde a un directorio válido.");
-            return new int[0];
         }
     }
 
     public void leerSubArchivos() {
-        int[] directorios_empleados = getEmpleadosDir();
+        File carpetaEmpleado = new File(carpeta);
+        File[] archivos = carpetaEmpleado.listFiles();
 
-        for (int directorio : directorios_empleados) {
-            empleadosDAOS.put(directorio, leerArchivo(directorio));
-            ConceptoDeduccionDAO dao_test = empleadosDAOS.get(directorio);
-            ConceptoDeduccion ultimo_dao = dao_test.obtener(dao_test.obtenerTodos().size() - 1);
-            String tipo_deduccion = ultimo_dao.getNombre();
-            System.out.println("Total de deducciones: " + dao_test.obtenerTodos());
-            System.out.println("Última deducción: " + tipo_deduccion);
+        if (archivos != null) {
+            for (File archivo : archivos) {
+                if (archivo.isFile()) {
+                    leerArchivo(Integer.parseInt(archivo.getName().split("\\.")[0]));
+                }
+            }
         }
-        System.out.println("Empleados y deducciones: " + empleadosDAOS);
-    }
-
-    public void agregarRegistro(int id_empleado, String nombre, double monto) {
-        ConceptoDeduccionDAO dao_empleado = seleccionarDAO(id_empleado);
-        dao_empleado.agregar(new ConceptoDeduccion(nombre, monto));
-    }
-
-    public void eliminarRegistro(int id_empleado, int index) {
-        ConceptoDeduccionDAO dao_empleado = seleccionarDAO(id_empleado);
-        dao_empleado.eliminar(index);
-    }
-
-    public void guardarCambios(int id_empleado) {
-        ConceptoDeduccionDAO dao_empleado = seleccionarDAO(id_empleado);
-        escribirArchivo(dao_empleado, id_empleado);
     }
 
     public ConceptoDeduccionDAO leerArchivo(int id_empleado) {
-        ConceptoDeduccionDAO dao_empleado = new ConceptoDeduccionDAO();
+        ConceptoDeduccionDAO dao = seleccionarDAO(id_empleado);
+        String archivoPath = carpeta + id_empleado + ".csv";
 
-        File archivo = new File(carpeta + id_empleado + "/deducciones.csv");
-        if (archivo.exists()) {
-            try {
-                Scanner scanner = new Scanner(archivo);
-                while (scanner.hasNextLine()) {
-                    String linea = scanner.nextLine();
-                    String[] campos = linea.split(",");
-
-                    if (campos.length == 2) {
-                        String nombre = campos[0];
-                        double monto = Double.parseDouble(campos[1]);
-
-                        dao_empleado.agregar(new ConceptoDeduccion(nombre, monto));
-                    }
-                }
-                scanner.close();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-        }
-
-        return dao_empleado;
-    }
-
-    public void escribirArchivo(ConceptoDeduccionDAO dao_empleado, int id_empleado) {
         try {
-            FileWriter fileWriter = new FileWriter(carpeta + id_empleado + "/deducciones.csv");
-            PrintWriter printWriter = new PrintWriter(fileWriter);
+            Scanner scanner = new Scanner(new File(archivoPath));
+            List<ConceptoDeduccion> deducciones = dao.getDeducciones();
 
-            List<ConceptoDeduccion> deducciones = dao_empleado.obtenerTodos();
-            for (ConceptoDeduccion deduccion : deducciones) {
-                printWriter.println(deduccion.getNombre() + "," + deduccion.getMonto());
+            deducciones.clear();
+
+            while (scanner.hasNextLine()) {
+                String linea = scanner.nextLine();
+                String[] valores = linea.split(",");
+
+                if (valores.length == 3) {
+                    int id = Integer.parseInt(valores[0]);
+                    String nombre = valores[1];
+                    double monto = Double.parseDouble(valores[2]);
+
+                    ConceptoDeduccion deduccion = new ConceptoDeduccion(id, nombre, monto);
+                    deducciones.add(deduccion);
+                }
             }
 
-            printWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("No se pudo encontrar el archivo para el empleado con ID: " + id_empleado);
+        }
+
+        return dao;
+    }
+
+    public void crearArchivosCSV() {
+        File carpetaEmpleados = new File(carpeta);
+        if (!carpetaEmpleados.exists()) {
+            carpetaEmpleados.mkdirs();
+        }
+
+        for (int id_empleado : empleadosDAOS.keySet()) {
+            ConceptoDeduccionDAO dao = empleadosDAOS.get(id_empleado);
+            String archivoPath = carpeta + id_empleado + ".csv";
+
+            try {
+                FileWriter fileWriter = new FileWriter(archivoPath);
+                PrintWriter printWriter = new PrintWriter(fileWriter);
+
+                for (ConceptoDeduccion deduccion : dao.getDeducciones()) {
+                    printWriter.println(deduccion.getId() + "," + deduccion.getNombre() + "," + deduccion.getMonto());
+                }
+
+                printWriter.close();
+                System.out.println("Archivo CSV creado para el empleado con ID: " + id_empleado);
+            } catch (IOException e) {
+                System.out.println("Error al crear el archivo CSV para el empleado con ID: " + id_empleado);
+            }
         }
     }
 
-    private void reverseFileArray(File[] files) {
-        int i = 0;
-        int j = files.length - 1;
-        File tmp;
+    public void agregarDeduccion(int id_empleado, ConceptoDeduccion deduccion) {
+        ConceptoDeduccionDAO dao = seleccionarDAO(id_empleado);
+        dao.crear(deduccion);
+    }
 
-        while (j > i) {
-            tmp = files[j];
-            files[j] = files[i];
-            files[i] = tmp;
-            j--;
-            i++;
-        }
+    public List<ConceptoDeduccion> obtenerDeducciones(int id_empleado) {
+        ConceptoDeduccionDAO dao = seleccionarDAO(id_empleado);
+        return dao.obtenerTodos();
+    }
+
+    public void eliminarDeduccion(int id_empleado, int id_deduccion) {
+        ConceptoDeduccionDAO dao = seleccionarDAO(id_empleado);
+        dao.eliminar(id_deduccion);
+    }
+
+    public void actualizarDeduccion(int id_empleado, ConceptoDeduccion deduccion) {
+        ConceptoDeduccionDAO dao = seleccionarDAO(id_empleado);
+        dao.actualizar(deduccion);
     }
 }
