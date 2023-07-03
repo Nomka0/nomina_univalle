31ccdb8b5c4d31067cbe0297d2059f33ea4679bf
Daniel <84422391+Daniel8900259@users.noreply.github.com>
2023-07-02 21:32:54 -0500

    Update ConceptoDeduccion.java

--- src/modelo/ConceptoDeduccion.java
+++ src/modelo/ConceptoDeduccion.java
@@ -6,11 +6,17 @@ import java.util.List;
 public class ConceptoDeduccion extends Entidad {
     private int codigo;
     private String nombre;
+    private int ficha;
+    private String fechaCorte;
+    private String tipoDeduccion;
+    private double valorDeduccion;
 
-    public ConceptoDeduccion(int codigo, String nombre) {
+    public ConceptoDeduccion(int codigo, String nombre, int ficha, String fechaCorte, String tipoDeduccion, double valorDeduccion) {
         super(codigo, nombre);
-        this.codigo = codigo;
-        this.nombre = nombre;
+        this.ficha = ficha;
+        this.fechaCorte = fechaCorte;
+        this.tipoDeduccion = tipoDeduccion;
+        this.valorDeduccion = valorDeduccion;
     }
 
     public ConceptoDeduccion() {
@@ -33,6 +39,38 @@ public class ConceptoDeduccion extends Entidad {
         this.nombre = nombre;
     }
 
+    public int getFicha() {
+        return ficha;
+    }
+
+    public void setFicha(int ficha) {
+        this.ficha = ficha;
+    }
+
+    public String getFechaCorte() {
+        return fechaCorte;
+    }
+
+    public void setFechaCorte(String fechaCorte) {
+        this.fechaCorte = fechaCorte;
+    }
+
+    public String getTipoDeduccion() {
+        return tipoDeduccion;
+    }
+
+    public void setTipoDeduccion(String tipoDeduccion) {
+        this.tipoDeduccion = tipoDeduccion;
+    }
+
+    public double getValorDeduccion() {
+        return valorDeduccion;
+    }
+
+    public void setValorDeduccion(double valorDeduccion) {
+        this.valorDeduccion = valorDeduccion;
+    }
+
     public double calcularValorDeduccion(double sumatoriaDevengos, double salarioMinimo) {
         // Deducción automática de salud y fondo de pensión
         if (sumatoriaDevengos > salarioMinimo) {
@@ -44,7 +82,7 @@ public class ConceptoDeduccion extends Entidad {
             return valorDeduccion;
         }
     }
-    
+
     public double sumatoria(ConceptoDevengoDAO sacarDevengos) {
         double sumatoriaDevengos = 0;
         List<ConceptoDevengo> devengosAnalizar = sacarDevengos.obtenerTodos();
