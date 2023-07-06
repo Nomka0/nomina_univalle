780d82e390f77cd167d8f7e7ce359eef27bd0219
leandroace <60167865+leandroace@users.noreply.github.com>
2023-07-05 23:53:04 -0500

    Main

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,27 +1,18 @@
 package app;
+
 import modelo.TarifaCana;
+import vista.EmpleadoVista;
 import modelo.ConceptoDevengo;
+import controlador.EmpleadoControlador;
 import dao.ConceptoDevengoDAO;
 
 public class Main {
 	public static void main(String[] args) {
-		//TarifaCana tarifa = new TarifaCana(); 
-		//tarifa.getTarifa(1, 'F');
-		
-		//testeando...
-		ConceptoDevengoDAO dao_devengos = new ConceptoDevengoDAO();
-		ConceptoDevengo corte_cana = new ConceptoDevengo(1234,"PRIMER PAGO SEMANAL","20230629");
-		corte_cana.corteCana(1, 'F');
-		
-		dao_devengos.crear(corte_cana);
-		
-		ConceptoDevengo uno_mas = dao_devengos.obtener(0);
-		
-		String nombre_del_devengo = uno_mas.getNombre();
-		
-		System.out.println("Este es el valor (pesos) del devengo creado: " + uno_mas.getValorDevengo());
-		
-		System.out.println("Este es el devengo creado: " + nombre_del_devengo);
+		// TarifaCana tarifa = new TarifaCana();
+		// tarifa.getTarifa(1, 'F');
+		EmpleadoVista vista = new EmpleadoVista();
+		EmpleadoControlador controlador = new EmpleadoControlador(vista);
+		vista.setVisible(true);
 
 	}
 }
