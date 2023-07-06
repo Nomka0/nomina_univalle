2e967e616dd1cf4b4c98b9d1d9a75652bc6abe50
Norione R <83548015+lxsae@users.noreply.github.com>
2023-07-05 23:39:06 -0500

    Funcionando

---
+++ src/controlador/CajaCompensacionController.java
@@ -0,0 +1,252 @@
+package controlador;
+
+import vista.CajaCompensacionVista;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+import javax.swing.table.DefaultTableModel;
+
+public class CajaCompensacionController {
+
+    private CajaCompensacionVista vista;
+    private static final String RUTA_BACKUP = "src/backup";
+
+    public CajaCompensacionController(CajaCompensacionVista vista) {
+        this.vista = vista;
+        this.vista.getBtnGuardar().addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                guardarDatosEnTabla();
+                try {
+                    crearArchivoBackup();
+                } catch (IOException ex) {
+                    Logger.getLogger(CajaCompensacionController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+
+        this.vista.getBtnListar().addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    listarDatos();
+                } catch (IOException ex) {
+                    Logger.getLogger(CajaCompensacionController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+
+        this.vista.getBtnEditar().addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                editarDatos();
+            }
+        });
+
+        this.vista.getBtnEliminar().addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                eliminarDatos();
+            }
+        });
+    }
+
+    private void guardarDatosEnTabla() {
+        String nombre = vista.getTxtNombreCajaCompensacion().getText();
+        String codigo = vista.getTxtCodigoCajaCompensacion().getText();
+        String fechaNacimiento = vista.getTxtFechaNacimiento().getText();
+        String fechaIngreso = vista.getTxtFechaIngreso().getText();
+        String fechaRetiro = vista.getTxtFechaRetiro().getText();
+        String tipoTrabajador = vista.getTxtTipoTrabajador().getText();
+        String tipoSalario = vista.getTxtTipoSalario().getText();
+        String numeroCuenta = vista.getTxtNumeroCuenta().getText();
+
+        DefaultTableModel model = (DefaultTableModel) vista.getTable().getModel();
+        model.addRow(
+                new Object[]{nombre, codigo, fechaNacimiento, fechaIngreso, fechaRetiro, tipoTrabajador, tipoSalario,
+                        numeroCuenta});
+
+        vista.getTxtNombreCajaCompensacion().setText("");
+        vista.getTxtCodigoCajaCompensacion().setText("");
+        vista.getTxtFechaNacimiento().setText("");
+        vista.getTxtFechaIngreso().setText("");
+        vista.getTxtFechaRetiro().setText("");
+        vista.getTxtTipoTrabajador().setText("");
+        vista.getTxtTipoSalario().setText("");
+        vista.getTxtNumeroCuenta().setText("");
+    }
+
+    private void crearArchivoBackup() throws IOException {
+        String nombreArchivo = "/cajacompensacion.txt";
+        String rutaArchivo = RUTA_BACKUP + nombreArchivo;
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(rutaArchivo))) {
+            DefaultTableModel model = (DefaultTableModel) vista.getTable().getModel();
+            int rowCount = model.getRowCount();
+            int columnCount = model.getColumnCount();
+
+            for (int i = 0; i < columnCount; i++) {
+                writer.write(model.getColumnName(i));
+                if (i < columnCount - 1) {
+                    writer.write("\t");
+                }
+            }
+            writer.newLine();
+
+            for (int row = 0; row < rowCount; row++) {
+                for (int col = 0; col < columnCount; col++) {
+                    Object value = model.getValueAt(row, col);
+                    writer.write(value.toString());
+                    if (col < columnCount - 1) {
+                        writer.write("\t");
+                    }
+                }
+                writer.newLine();
+            }
+
+            System.out.println("Archivo de backup creado exitosamente.");
+        }
+    }
+
+    private void listarDatos() throws IOException {
+        // Implementa la lógica para listar los datos en la vista
+            try (BufferedReader reader = new BufferedReader(new FileReader(RUTA_BACKUP + "/cajacompensacion.txt"))) {
+            DefaultTableModel model = (DefaultTableModel) vista.getTable().getModel();
+            model.setRowCount(0); // Limpiar la tabla antes de listar los datos
+
+            // Leer la primera línea para obtener los nombres de las columnas
+            String columnLine = reader.readLine();
+            String[] columnNames = columnLine.split("\t");
+            model.setColumnIdentifiers(columnNames);
+
+            // Leer las líneas restantes para obtener los datos
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split("\t");
+                model.addRow(data);
+            }
+                }
+    }
+
+        private void editarDatos() {
+        int selectedRow = vista.getTable().getSelectedRow();
+        if (selectedRow == -1) {
+            JOptionPane.showMessageDialog(vista, "Selecciona una fila para editar.", "Error", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        String nombre = vista.getTxtNombreCajaCompensacion().getText();
+        String codigo = vista.getTxtCodigoCajaCompensacion().getText();
+        String fechaNacimiento = vista.getTxtFechaNacimiento().getText();
+        String fechaIngreso = vista.getTxtFechaIngreso().getText();
+        String fechaRetiro = vista.getTxtFechaRetiro().getText();
+        String tipoTrabajador = vista.getTxtTipoTrabajador().getText();
+        String tipoSalario = vista.getTxtTipoSalario().getText();
+        String numeroCuenta = vista.getTxtNumeroCuenta().getText();
+
+        DefaultTableModel model = (DefaultTableModel) vista.getTable().getModel();
+        model.setValueAt(nombre, selectedRow, 0);
+        model.setValueAt(codigo, selectedRow, 1);
+        model.setValueAt(fechaNacimiento, selectedRow, 2);
+        model.setValueAt(fechaIngreso, selectedRow, 3);
+        model.setValueAt(fechaRetiro, selectedRow, 4);
+        model.setValueAt(tipoTrabajador, selectedRow, 5);
+        model.setValueAt(tipoSalario, selectedRow, 6);
+        model.setValueAt(numeroCuenta, selectedRow, 7);
+
+        // Actualizar el archivo de backup con los nuevos datos
+        actualizarArchivoBackup();
+    }
+/*
+    private void actualizarArchivoBackup() {
+        String rutaArchivo = RUTA_BACKUP + "/cajacompensacion.txt";
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(rutaArchivo))) {
+            DefaultTableModel model = (DefaultTableModel) vista.getTable().getModel();
+            int rowCount = model.getRowCount();
+            int columnCount = model.getColumnCount();
+
+            // Escribir los encabezados de las columnas en el archivo
+            for (int i = 0; i < columnCount; i++) {
+                writer.write(model.getColumnName(i));
+                if (i < columnCount - 1) {
+                    writer.write("\t");
+                }
+            }
+            writer.newLine();
+
+            // Escribir los datos de cada fila en el archivo
+            for (int row = 0; row < rowCount; row++) {
+                for (int col = 0; col < columnCount; col++) {
+                    Object value = model.getValueAt(row, col);
+                    writer.write(value.toString());
+                    if (col < columnCount - 1) {
+                        writer.write("\t");
+                    }
+                }
+                writer.newLine();
+            }
+
+            System.out.println("Archivo de backup actualizado exitosamente.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+*/
+
+    
+
+        private void eliminarDatos() {
+        int selectedRow = vista.getTable().getSelectedRow();
+        if (selectedRow == -1) {
+            JOptionPane.showMessageDialog(vista, "Selecciona una fila para eliminar.", "Error", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        DefaultTableModel model = (DefaultTableModel) vista.getTable().getModel();
+        model.removeRow(selectedRow);
+
+        // Actualizar el archivo de backup después de eliminar los datos
+        actualizarArchivoBackup();
+    }
+
+    private void actualizarArchivoBackup() {
+        String rutaArchivo = RUTA_BACKUP + "/cajacompensacion.txt";
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(rutaArchivo))) {
+            DefaultTableModel model = (DefaultTableModel) vista.getTable().getModel();
+            int rowCount = model.getRowCount();
+            int columnCount = model.getColumnCount();
+
+            // Escribir los encabezados de las columnas en el archivo
+            for (int i = 0; i < columnCount; i++) {
+                writer.write(model.getColumnName(i));
+                if (i < columnCount - 1) {
+                    writer.write("\t");
+                }
+            }
+            writer.newLine();
+
+            // Escribir los datos de cada fila en el archivo
+            for (int row = 0; row < rowCount; row++) {
+                for (int col = 0; col < columnCount; col++) {
+                    Object value = model.getValueAt(row, col);
+                    writer.write(value.toString());
+                    if (col < columnCount - 1) {
+                        writer.write("\t");
+                    }
+                }
+                writer.newLine();
+            }
+
+            System.out.println("Archivo de backup actualizado exitosamente.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/dao/CajaCompensacionDAO.java
@@ -0,0 +1,103 @@
+/*
+package dao;
+import java.util.ArrayList;
+import java.util.List;
+import modelo.CajaCompensacion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CajaCompensacionDAO {
+    private List<CajaCompensacion> cajas;
+
+    public CajaCompensacionDAO() {
+        cajas = new ArrayList<>();
+    }
+
+    public void agregarCajaCompensacion(CajaCompensacion caja) {
+        cajas.add(caja);
+    }
+
+    public void eliminarCajaCompensacion(CajaCompensacion caja) {
+        cajas.remove(caja);
+    }
+
+    public List<CajaCompensacion> obtenerCajasCompensacion() {
+        return cajas;
+    }
+}
+*/
+package dao;
+
+import modelo.CajaCompensacion;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class CajaCompensacionDAO {
+    private List<CajaCompensacion> cajas;
+
+    public CajaCompensacionDAO() {
+        cajas = new ArrayList<>();
+    }
+
+    public void agregarCaja(CajaCompensacion caja) {
+        cajas.add(caja);
+    }
+
+    public void eliminarCaja(CajaCompensacion caja) {
+        cajas.remove(caja);
+    }
+
+    public List<CajaCompensacion> obtenerCajas() {
+        return cajas;
+    }
+
+    public CajaCompensacion buscarCajaPorCodigo(String codigo) {
+        for (CajaCompensacion caja : cajas) {
+            if (caja.getCodigo().equals(codigo)) {
+                return caja;
+            }
+        }
+        return null; // Si no se encuentra la caja
+    }
+
+    public List<CajaCompensacion> buscarCajasPorFechaIngreso(Date fechaIngreso) {
+        List<CajaCompensacion> cajasEncontradas = new ArrayList<>();
+        for (CajaCompensacion caja : cajas) {
+            if (caja.getFechaIngreso().equals(fechaIngreso)) {
+                cajasEncontradas.add(caja);
+            }
+        }
+        return cajasEncontradas;
+    }
+
+    // Otros métodos de consulta o actualización de la base de datos podrían ir aquí
+
+    // Ejemplo de uso del DAO
+    public static void main(String[] args) {
+        CajaCompensacionDAO dao = new CajaCompensacionDAO();
+
+        // Crear una nueva caja
+        CajaCompensacion caja1 = new CajaCompensacion("001", "Caja 1", new Date(), new Date(), null, "Tipo 1",
+                "Salario 1", "123456789");
+        dao.agregarCaja(caja1);
+
+        // Obtener todas las cajas
+        List<CajaCompensacion> cajas = dao.obtenerCajas();
+        System.out.println("Cajas disponibles:");
+        for (CajaCompensacion caja : cajas) {
+            System.out.println(caja.getNombre());
+        }
+
+        // Buscar una caja por código
+        String codigoBuscado = "001";
+        CajaCompensacion cajaEncontrada = dao.buscarCajaPorCodigo(codigoBuscado);
+        if (cajaEncontrada != null) {
+            System.out.println("Caja encontrada: " + cajaEncontrada.getNombre());
+        } else {
+            System.out.println("Caja no encontrada");
+        }
+    }
+}
